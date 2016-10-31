32921507ab42cd772206f6e4c292925e6fb526d2
Deepankur <deepankur@Deepankurs-MacBook-Pro.local>
2016-11-01 03:55:31 +0530

    animation working

--- framelayout/ViewController.swift
+++ framelayout/ViewController.swift
@@ -19,19 +19,15 @@ class ViewController: UIViewController ,UIScrollViewDelegate{
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    let screenSize: CGRect = UIScreen.main.bounds
     func greet(person: String) -> String {
         var scrollView: UIScrollView
         var imageView: UIImageView
-        let screenSize: CGRect = UIScreen.main.bounds
         
         imageView = UIImageView(image: UIImage(named: "image.png"))
-        
         scrollView = UIScrollView(frame: view.bounds)
         scrollView.backgroundColor = UIColor.lightGray
-       // scrollView.contentSize = imageView.bounds.size
         scrollView.autoresizingMask = [ UIViewAutoresizing.flexibleWidth , UIViewAutoresizing.flexibleHeight]
-        //scrollView.addSubview(imageView)
         view.addSubview(scrollView)
         
         let topMargin : Int=generateAndAddTabs(scrollView: scrollView)
@@ -48,42 +44,80 @@ class ViewController: UIViewController ,UIScrollViewDelegate{
     
     var viewList = [UIImageView]()
     
-    let TABS_TO_ADD : Int=4;
-    let TOTAL_HEIGHT_OF_TAB:Int = 40;
-    let SCROLL_VIEW_TOP_MARGIN:Int = 50;
-    let VERTICAL_TAB_MARGIN :Int = 10;
+    let TAB_COUNT : Int=10;
+    let TOTAL_HEIGHT_OF_TAB:Int = 50;
+    //let SCROLL_VIEW_TOP_MARGIN:Int = 50;
+    
     
     func generateAndAddTabs(scrollView : UIScrollView) -> Int {
         
         let screenSize: CGRect = UIScreen.main.bounds
-        var topMargin: Int=0;
+        var topMargin: Int = (TAB_COUNT-1) * TOTAL_HEIGHT_OF_TAB;
         
         
-        for i in 0 ..< 6 {            
+        for i in 0 ..< TAB_COUNT {
             let imageView:UIImageView
-            
-            imageView = UIImageView(frame: CGRect(x: 0, y: topMargin, width: Int(screenSize.width - 0), height: 50))
+            imageView = UIImageView(frame: CGRect(x: 0, y: topMargin, width: Int(screenSize.width - 0), height: TOTAL_HEIGHT_OF_TAB))
             imageView.image = UIImage(named: "glossy")
             
-            topMargin=topMargin+50;
+            topMargin=topMargin-TOTAL_HEIGHT_OF_TAB;
             scrollView.contentSize=CGSize(width:screenSize.width,height : CGFloat(topMargin))
             addToList(im: imageView)
             scrollView.addSubview(imageView)
-            
         }
-        return topMargin;
+        return TAB_COUNT * TOTAL_HEIGHT_OF_TAB;
     }
     
     func addToList(im: UIImageView)  {
         viewList.append( im)
-        if viewList.count == 3 {
-          //  im.frame = CGRect(x: 100,y: 000,width: 100,height: 600 ); // set new position exactly
-        }
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let f: CGFloat=scrollView.contentOffset.y
         NSLog(" %@",  f.description);
+        resetTopMargins(currentOffset: f)
     }
+    
+    func resetTopMargins(currentOffset: CGFloat)  {
+        for i in 0..<viewList.count {
+            let imageView:UIImageView = viewList[i];
+            let translationForLoopingTab:Int=getMarginForTab(index: i, scrollY: Int(currentOffset))
+            
+            if i>0 {
+            imageView.frame = CGRect(x: 0,y: translationForLoopingTab,width: Int(screenSize.width),height: TOTAL_HEIGHT_OF_TAB );
+            }
+        }
+    }
+    
+    func getOriginalTopMargin(index:Int) -> Int{
+    return (TOTAL_HEIGHT_OF_TAB*(TAB_COUNT - index-1))
+    }
+    
+    func  getMarginForTab( index :Int, scrollY :Int)->Int {
+        var translation :Int = 0;
+        
+        if index == 0 {
+            translation = 0
+            
+        }
+        else {
+            if scrollY < getCutOffTranslationForTab(index:index){
+                translation = scrollY;
+            }
+            else{
+                translation = getCutOffTranslationForTab(index:index);
+            }
+        }
+        return translation+getOriginalTopMargin(index: index);
+    }
+    
+    func  getCutOffTranslationForTab( index:Int) ->Int{
+        if index == 0{
+        return 0;
+    }
+    return (TOTAL_HEIGHT_OF_TAB ) * index;
+    }
+    
+    
 }
 
